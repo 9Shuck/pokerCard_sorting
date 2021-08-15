e6b7516b96995ac11a6265265957aab8a6e75956
Rubén Pérez <54080189+9Shuck@users.noreply.github.com>
2021-08-15 11:42:26 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,93 +1,124 @@
-const DRAW = document.querySelector("#draw");
-const BUBLE = document.querySelector("#buble");
-const SORT = document.querySelector("#sort");
-const inputCARDS = document.querySelector("#nCards");
-const cardContainer = document.querySelector("#cardContainer");
-const HIDDEN = document.querySelector(".botoncicos");
+let valueCard = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let auxiliarVar = [];
+let auxilirSym = [];
+let arrayVal = [];
+let arraySym = [];
+let cardSymb = ["Heart", "Club", "Spade", "Diamond"];
 
-const FORM = document.querySelector("#form");
-const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const SYMBOLS = ["♦", "♥", "♠", "♣"];
+document.querySelector("#briefeBtn").addEventListener("click", function() {
+  document.querySelector("#cardContainer").innerHTML = "";
+  document.querySelector("#cardSortContainer").innerHTML = "";
+  let cantidad = document.querySelector("#amountInput").value;
+  let valueIndex = 0;
+  let symbolIndex = 0;
+  for (let i = 0; i < cantidad; i++) {
+    valueIndex = Math.floor(Math.random() * 13);
+    symbolIndex = Math.floor(Math.random() * 4);
+    document.querySelector("#cardContainer").innerHTML += generateCard(
+      valueCard[valueIndex],
+      cardSymb[symbolIndex]
+    );
+    auxiliarVar.push(valueIndex);
+    auxilirSym.push(symbolIndex);
+  }
+  arrayVal = auxiliarVar;
+  arraySym = auxilirSym;
+});
 
-window.onload = function() {
-  sendNumb();
-};
+document.querySelector("#bubleBtn").addEventListener("click", function() {
+  document.querySelector("#cardSortContainer").innerHTML = "";
+  bubbleSort(arrayVal, arraySym);
+});
 
-const cardGenerator = num => {
-  for (let u = 0; u < num; u++) {
-    cardContainer.appendChild.innerHTML = "<div class='cartag'></div>";
+document.querySelector("#selectBtn").addEventListener("click", function() {
+  document.querySelector("#cardSortContainer").innerHTML = "";
+  selectSort(arrayVal, arraySym);
+});
+
+function generateCard(value, icon) {
+  return `<div class="cardWrapper">
+                <div class="card">
+                    <div class="topCard">
+                        <span class="card${icon}"></span>
+                    </div>
+                    <div class="middleCard">
+                        <span class="middleNumber">${value}</span>
+                    </div>
+                    <div class="bottomCard">
+                        <span class="card${icon}"></span>
+                    </div>
+                </div>
+            </div>`;
+}
+
+const bubbleSort = (arr, arr2) => {
+  document.querySelector(".ninja").style.visibility = "visible";
+  let wall = arr.length - 1;
+  while (wall > 0) {
+    let index = 0;
+    let index2 = 0;
+    let aux = 0;
+    let aux2 = 0;
+    while (index < wall) {
+      if (arr[index] > arr[index + 1]) {
+        aux = arr[index];
+        arr[index] = arr[index + 1];
+        arr[index + 1] = aux;
+        aux2 = arr2[index2];
+        arr2[index2] = arr2[index2 + 1];
+        arr2[index2 + 1] = aux2;
+      }
+      index++;
+      index2++;
+    }
+    wall--;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    document.querySelector("#cardSortContainer").innerHTML += generateCard(
+      valueCard[arr[i]],
+      cardSymb[arr2[i]]
+    );
   }
 };
-const cardArrayGenerator = numero => {
-  let selCard = [];
 
-  for (let i = 0; i < numero; i++) {
-    selCard.push([]);
-    console.log("Bucle en vuelta numero: " + i + "array:" + selCard);
-    selCard[i][0] = Math.floor(Math.random() * NUMBERS.length);
-    selCard[i][1] = Math.floor(Math.random() * SYMBOLS.length);
-  }
-  return selCard;
-};
-
-const sendNumb = () => {
-  FORM.addEventListener("submit", event => {
-    event.preventDefault();
-    let numerovalidado = isValidNumero();
-    cardGenerator(numerovalidado);
-    HIDDEN.style.visibility = "visible";
-  });
-};
-
-const isValidNumero = () => {
-  if (inputCARDS.value.length < 10 && inputCARDS.value > 0) {
-    return inputCARDS.value;
-  } else {
-    console.log("falloooo");
-  }
-};
-
-const sortBuble = array => {
-  BUBLE.addEventListener("click", () => {
-    return bubbleSortARR(array);
-  });
-};
-
-const sortSelect = array => {
-  BUBLE.addEventListener("click", () => {
-    return selectionSortARR(array);
-  });
-};
-
-const selectionSortARR = inputArr => {
-  let n = inputArr.length;
-
-  for (let i = 0; i < n; i++) {
-    let min = i;
-    for (let j = i + 1; j < n; j++) {
-      if (inputArr[j][0] < inputArr[min][0]) {
-        min = j;
+const selectSort = (arr, arr2) => {
+  document.querySelector("#cardSortContainer").innerHTML = "";
+  document.querySelector(".ninja").style.visibility = "visible";
+  let min = 0;
+  let aux = 0;
+  let aux2 = 0;
+  while (min < arr.length - 1) {
+    for (let i = min + 1; i < arr.length; i++) {
+      if (arr[min] > arr[i]) {
+        aux = arr[min];
+        arr[min] = arr[i];
+        arr[i] = aux;
+        aux2 = arr2[min];
+        arr2[min] = arr2[i];
+        arr2[i] = aux2;
       }
     }
-    if (min != i) {
-      let tmp = inputArr[i];
-      inputArr[i] = inputArr[min];
-      inputArr[min] = tmp;
-    }
+    min++;
   }
-  return inputArr;
+  for (let i = 0; i < arr.length; i++) {
+    document.querySelector("#cardSortContainer").innerHTML += generateCard(
+      valueCard[arr[i]],
+      cardSymb[arr2[i]]
+    );
+  }
 };
 
-const bubbleSortARR = inputArr => {
-  let len = inputArr.length;
-  for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len; j++) {
-      if (inputArr[j][0] > inputArr[j + 1]) {
-        let tmp = inputArr[j];
-        inputArr[j] = inputArr[j + 1];
-        inputArr[j + 1] = tmp;
-      }
-    }
-  }
-  return inputArr;
-};
